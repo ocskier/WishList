e55224035125f96393f012d2292b594589fff59b
Pack Fan 1031 <ocskier@gmail.com>
2018-11-05 12:38:41 -0500

    fixing deploy

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 	console.log('YOU ARE IN THE PRODUCTION ENV');
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, './server/client/build/'))
+		res.sendFile(path.join(__dirname, '../client/build/'))
 	});
 }
 
