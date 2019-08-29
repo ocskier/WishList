05a96d02db63fc6c56370ab98ca6cdc8d468d2d6
Jon Jackson <ocskier@gmail.com>
2019-08-29 10:30:39 -0400

    inconsequential changes

--- server/scripts/seedDB.js
+++ server/scripts/seedDB.js
@@ -7,53 +7,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglis
 
 const giftSeed = [
   {
-    name: "The Dead Zone",
-    date: new Date(Date.now())
-  },
-  {
-    name: "Lord of the Flies",
-    date: new Date(Date.now())
-  },
-  {
-    name: "The Catcher in the Rye",
-    date: new Date(Date.now())
-  },
-  {
-    name: "The Punch Escrow",
-    date: new Date(Date.now())
-  },
-  {
-    name: "Harry Potter and the Sorcerer's Stone",
-    date: new Date(Date.now())
-  },
-  {
-    name: "Coraline",
-    date: new Date(Date.now())
-  },
-  {
-    name: "Code: The Hidden Language of Computer Hardware and Software",
-    date: new Date(Date.now())
-  },
-  {
-    name: "The Everything Store: Jeff Bezos and the Age of Amazon",
-    date: new Date(Date.now())
-  },
-  {
-    name: "Total Recall: My Unbelievably True Life Story",
-    date: new Date(Date.now())
-  },
-  {
-    name: "Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future",
-    date: new Date(Date.now())
-  },
-  {
-    name: "Steve Jobs",
-    date: new Date(Date.now())
-  }
-];
-
-const listSeed = [
-  {
     user: "ocskier",
     // date: new Date(Date.now())
     gift: "Scooter"
